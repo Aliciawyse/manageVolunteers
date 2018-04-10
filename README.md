5f1cce9e3de641526bf83796a120995329fcdc50
Barrett <Aliciawyse@users.noreply.github.com>
2018-04-09 23:55:57 -0400

    first commit

---
+++ README.md
@@ -0,0 +1,44 @@
+# Volunteer project
+This is a prototype of a simple full stack web app for a nonprofit group to manage users. 
+
+## Authors
+
+* [**Alicia Barrett**](https://github.com/Aliciawyse)
+* [**Jon Schwenn**](https://twitter.com/jonschwenn)
+
+
+## Built with
+
+* JavaScript
+* [Jade](http://jade-lang.com/)
+* [Node.js](https://nodejs.org/en/)
+* [Express](https://expressjs.com/)
+* MySQL
+* GET and POST routes for retrieving and adding new volunteer data
+
+More details coming soon. 
+
+## Development Process
+
+* [1. Concept](#1-concept)
+* [2. Process](#2-process)
+* [3. Challenges and Successes!](#3-challenges-and-successes)
+* [4. Future Additions](#4-future-additions)
+
+
+### 1. Concept
+
+Offer an easy way to manage users.   
+
+### 2. Process
+
+To get started, I followed this [step-by-step guide](https://closebrace.com/tutorials/2017-03-02/the-dead-simple-step-by-step-guide-for-front-end-developers-to-getting-up-and-running-with-nodejs-express-and-mongodb) by [Christopher Buecheler](https://closebrace.com/u/cwbuecheler) to getting up and running with Node.js, Express and MongoDB. I opted to use MySQL.
+
+### 3. Challenges and successes
+
+Some challenges included...
+        
+
+### 4. Future additions
+
+Details coming soon. 
---
+++ routes/index.js
@@ -0,0 +1,59 @@
+var express = require('express');
+var router = express.Router();
+
+// /* GET home page. */
+// router.get('/', function(req, res, next) {
+//   res.render('index', { title: 'Express' });
+// });
+//
+// //GET hello world page
+// router.get('/helloworld', function(req,res){
+//   res.render('helloworld',{title: 'Hello World!'})
+// });
+
+/* GET Userlist page. */
+router.get('/userlist', function(req, res) {
+
+  var connection = req.db;
+
+    connection.query('SELECT * FROM volunteers', function(err, rows, fields) {
+        res.render('userlist', {
+            "userlist" : rows,
+        });
+    });
+});
+
+/* GET New User page. */
+router.get('/newuser', function(req, res) {
+    res.render('newuser', { title: 'Add New User' });
+});
+
+/* POST to Add User Service */
+router.post('/adduser', function(req, res) {
+
+    // Set our internal DB variable
+    var connection = req.db;
+
+    // Get our form values. These rely on the "name" attributes
+    var userName = req.body.username;
+    //var userEmail = req.body.useremail;
+
+    // We're not populating ID here because it should be auto-incrementing
+    var newUser = {firstName: userName};
+
+    // Submit to the DB
+    connection.query('INSERT INTO volunteers SET ?', newUser, function (error, results, fields) {
+        if (error) {
+            // If it failed, return error
+            res.send("There was a problem adding the information to the database.");
+        }
+        else {
+            // If it worked, set the header so the address bar doesn't still say /adduser
+            //res.location("userlist");
+            // And forward to success page
+            res.redirect("userlist");
+        }
+    });
+});
+
+module.exports = router;
